27ff91c8355695ca934d62134c199882c6827a2b
Martijn van Maurik <martijn@vmaurik.nl>
2014-08-08 13:42:31 +0200

    Add ipv6 possibility

--- scripts/docker-ts3.sh
+++ scripts/docker-ts3.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 VOLUME=/teamspeak3
 
+if [ ! -z "$IPV6ADDR" ]; then
+	echo  $IPV6ADDR
+	ip -6 addr add "$IPV6ADDR" dev eth0
+fi
+
+sleep 2
+
+if [ ! -z "$IPV6GW" ]; then
+	echo $IPV6GW
+	ip -6 route add  default via "$IPV6GW" dev eth0
+fi
+
 echo " ----- docker-ts3 ------"
 echo "1. Check if ts3server.sqlitedb exists in host-mounted volume."
 if [ -f $VOLUME/ts3server.sqlitedb ]
